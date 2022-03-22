b5a4e7cff1f2d5add3b7a9a8037e8763c48dad54
Faye <fayebheevayne@gmail.com>
2022-03-22 21:07:10 +0800

    updated pages

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-    </head>
-    <body>
+@section('content')
         <h1>Welcome to my site</h1>
         <p>this website is cerated using laravel</p>
-    </body>
-</html>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('content')
         <h1>Services</h1>
         <p>this is services section</p>
-
+@endsection
