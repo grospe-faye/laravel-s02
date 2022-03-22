2bc36a3e34daebf94529f5da1f69e5743f22c1a5
Faye <fayebheevayne@gmail.com>
2022-03-22 18:50:46 +0800

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function(){
-    return view('hello');
-});
+// Route::get('/hello', function(){
+//     return view('hello');
+// });
+
+Route::get('/hello', 'PageController@hello')
