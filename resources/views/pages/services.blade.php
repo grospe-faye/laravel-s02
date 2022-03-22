f5af1f9beec5f81f2e676564384b296caee13b5a
Faye <fayebheevayne@gmail.com>
2022-03-22 21:01:47 +0800

    updated layouts

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-    </head>
-    <body>
         <h1>Services</h1>
         <p>this is services section</p>
-    </body>
-</html>
+
