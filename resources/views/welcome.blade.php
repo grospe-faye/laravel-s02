aa96bf93a3ccf95fc571a4ecc6d47c42225dffd6
Faye <fayebheevayne@gmail.com>
2022-03-22 17:59:36 +0800

    updated welcome.blade.php in views folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHP Laravel
                 </div>
 
                 <div class="links">
