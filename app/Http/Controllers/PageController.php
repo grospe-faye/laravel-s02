384941fe371c2c47dee9099fe9789568e00f07d5
Faye <fayebheevayne@gmail.com>
2022-03-22 20:59:22 +0800

    added new folder layouts

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // passing a single value to a view
+    /*public function hello(){
+        return view('hello')->with('name', 'Homer Simpson');
+    }*/
+
+    // Passing multiple values
+    public function hello(){
+        $info = array(
+            'front_end' => 'Zuitt Coding Bootcamp',
+            'topics' => ['HTML & CSS', 'JS', 'React']
+        );
+        return view('hello')->with($info);
+    }
+
+    public function index(){
+        return view('pages/index');
+    }
+
+    public function about(){
+        return view('pages/about');
+    }
+
+    public function services(){
+        return view('pages/services');
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <h1>Welcome to my site</h1>
+        <p>this website is cerated using laravel</p>
+    </body>
+</html>
