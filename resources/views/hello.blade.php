1cf4013c93b02d8bd19be2b931f8beb1627e952e
Faye <fayebheevayne@gmail.com>
2022-03-22 20:26:39 +0800

    updated hello.blade.php ang pagecontroller

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,1 +1,10 @@
-<h1>Hello World!</h1>
+<h1>Hello World from page controller</h1>
+<p>{{ $front_end }}</p>
+
+@if(count($topics) > 0)
+	@foreach($topics as $topic)
+		<li>{{ $topic }}</li>
+	@endforeach
+@else
+<p>nothing to display</p>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 //     return view('hello');
 // });
 
-Route::get('/hello', 'PageController@hello')
+Route::get('/hello', 'PageController@hello');
