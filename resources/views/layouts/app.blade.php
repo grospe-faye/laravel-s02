2d495fe9e4c203be1e037aa4eb31c7b27937fddb
Faye <fayebheevayne@gmail.com>
2022-03-23 18:23:05 +0800

    updated app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="{{asset('/css/app.css')}}">
 
         <title>Laravel</title>
 
     </head>
     <body>
-        @yield('content')
+       <div class="container">
+            @yield('content')
+       </div>
     </body>
 </html>
