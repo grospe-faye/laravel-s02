4069e9f65cef9746b787fd871a12fa2ac5dc05b9
Faye <fayebheevayne@gmail.com>
2022-04-14 17:45:40 +0800

    updated title

--- resources/views/layouts/app_old.blade.php
+++ resources/views/layouts/app_old.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="{{asset('/css/app.css')}}">
 
-        <title>Laravel</title>
+        <title>Kwaderno</title>
 
     </head>
     <body>
