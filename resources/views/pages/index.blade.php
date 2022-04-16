97aad4fd5322acf227d54a249c455cba24ac03db
Faye <fayebheevayne@gmail.com>
2022-04-16 19:29:49 +0800

    updated content in index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
         <h1>{{ $title }}</h1>
-        <p>this website is created using laravel</p>
+        <p>An online journal where users may document their day-to-day experiences and express themselves through writing. Created using PHP, MySQL, and Laravel Framework, users can create, view, update and delete their posts.</p>
 @endsection
