7d55149940f2cfa45a894139a84710585b0e3a34
Faye <fayebheevayne@gmail.com>
2022-04-17 16:38:01 +0800

    updated welcome message

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,5 @@
 @section('content')
         <h1 class="text-center">{{ $title }}</h1>
         <p class="text-center">An online journal where you can document your day-to-day experiences and express yourself through writing.</p>
-        <p class="text-center"> Grow a plant through writing!</p>
+        
 @endsection
