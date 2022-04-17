41477995178989a786c23402a1c7783859094556
Faye <fayebheevayne@gmail.com>
2022-04-17 16:11:36 +0800

    updated welcome message

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>{{ $title }}</h1>
-        <p>An online journal where you can document your day-to-day experiences and express yourself through writing.</p>
+        <h1 class="text-center">{{ $title }}</h1>
+        <p class="text-center">An online journal where you can document your day-to-day experiences and express yourself through writing.</p>
 @endsection
