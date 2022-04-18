ba9a5b92c91ba28195d99ebb80bc36fc5bb2f68c
Faye <fayebheevayne@gmail.com>
2022-04-18 10:33:14 +0800

    updates

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,23 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-        
-        <div class="split left" >
-                 <div class="centered">
-                        <h1>{{ $title }}</h1>
-                         <p>An online journal where you can document your day-to-day experiences and express yourself through writing.</p>
+        <div class="container">
+                <div class="row">
+                        
+                        <div class="col">
+                                <img src="https://images.squarespace-cdn.com/content/5e7e6ab5b449574311fd2187/1603745398485-NJ1PPM6K7UNOLETY02DG/White%2BMini%2BA6%2BNotebook%2BMinimal%2BGrid%2BJournal%2BNotepad%2BPU%2Bleather%2B_%2BEtsy.jpg?content-type=image%2Fjpeg" class="img-fluid" fill="none" alt="notebook">
+                        </div>
+                        <div class="col">
+                                 <h1>{{ $title }}</h1>
+                                 <p>An online journal where you can document your day-to-day experiences and express yourself through writing.</p>
+                        </div>
                 </div>
         </div>
-
-        <div class="split right" style="float:right;">
-                <div class="centered">
-                        <img src="https://img.freepik.com/free-photo/organic-paper-notebook-with-green-twig-white-background-zero-waste-concept-great-bussines-idea-recycle-concept-white-isolate-eco-friendly_205121-858.jpg?w=996" alt="notebook">
-   
-                </div>
-        </div>
-        <!-- 
-        
-        <img src="https://images.squarespace-cdn.com/content/5e7e6ab5b449574311fd2187/1603745398485-NJ1PPM6K7UNOLETY02DG/White%2BMini%2BA6%2BNotebook%2BMinimal%2BGrid%2BJournal%2BNotepad%2BPU%2Bleather%2B_%2BEtsy.jpg?content-type=image%2Fjpeg" alt="notebook"> -->
-
-        
 @endsection
