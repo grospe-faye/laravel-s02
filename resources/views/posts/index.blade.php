810822317c250e93236ef3e0ba4b87b4a7e9c2b4
Faye <fayebheevayne@gmail.com>
2022-04-21 16:05:07 +0800

    updates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,11 @@
 <h1>Posts</h1>
 	@if(count($posts) > 0)
 	@foreach($posts as $post)
-		<div  class="card bg-light mb-3" style="max-width: 18rem;">
+		<div class="card bg-light mb-3" style="max-width: 18rem;">
 			<div class="card-header">
 			</div>
 				<div class="card-body">
-						<h5 class="card-title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h5>
+						<h4 class="card-title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h4>
 						<p class="card-text">{{$post->body}}</p>
 						<p class="card-text"><small class="text-muted">Written on {{$post->created_at}}</small></p>
 					{{ $posts->links() }}
