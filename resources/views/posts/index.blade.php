da2f0d13bcf2808ae32b9ad36ce13e110933e48a
Faye <fayebheevayne@gmail.com>
2022-04-21 15:48:06 +0800

    updates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card-columns">
-	<h1>Posts</h1>
-
-
-	<div class="card">
+<h1>Posts</h1>
 	@if(count($posts) > 0)
-		@foreach($posts as $post)
-    	<div class="card-body">
-     	 <h5 class="card-title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h5>
-     	 <p class="card-text">{{$post->body}}</p>
-      	 <p class="card-text"><small class="text-muted">Written on {{$post->created_at}}</small></p>
+	@foreach($posts as $post)
+		<div class="card border-light mb-3" style="max-width: 18rem;">
+			<div class="card-header">
+				<h5 class="card-title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></h5>
+			</div>
+				<div class="card-body">
+						
+						<p class="card-text">{{$post->body}}</p>
+						<p class="card-text"><small class="text-muted">Written on {{$post->created_at}}</small></p>
+					{{ $posts->links() }}
+				</div>
+			</div>
 		</div>
-		@endforeach
-		{{ $posts->links() }}
-    </div>
-</div>
-
+@endforeach
 	@else
 		<p>No posts found</p>
 	@endif
