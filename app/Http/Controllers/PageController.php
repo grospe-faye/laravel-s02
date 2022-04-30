52bef89bee12ed803615f3a7d52ff8fbf48b12ee
Faye <fayebheevayne@gmail.com>
2022-04-30 09:43:06 +0800

    updates

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -21,7 +21,7 @@ class PageController extends Controller
     }
 
     public function index(){
-        $title = 'Kwaderno!';
+        $title = 'Kwaderno';
         return view('pages/index')->with('title', $title);
     }
 
