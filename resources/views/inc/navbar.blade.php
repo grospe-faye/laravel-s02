d75bf350beee7544164ec9cbe388c494b53ea123
Faye <fayebheevayne@gmail.com>
2022-05-01 14:25:59 +0800

    updates

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,3 +1,26 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- <title>{{ config('app.name', 'Kwaderno') }}</title> -->
+    <title>Kwaderno</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
 <nav class="navbar navbar-expand-lg mb-3 navbar navbar-light" style="background-color: #b99378;">
   <a class="navbar-brand" href="#" style="color: #FFEBCC;">Kwaderno</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -44,3 +67,6 @@
                           @endguest                         
   </ul>
 </nav>
+
+</body>
+</html>
